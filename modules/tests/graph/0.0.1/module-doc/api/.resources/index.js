9f0ea5c8ef2665ac101a341df66b2086be5f5d9c
Thorsten Seitz <thorsten@portableinnovations.de>
2015-01-22 20:36:32 +0100

    Updated API docs.

---
+++ modules/tests/graph/0.0.1/module-doc/api/.resources/index.js
@@ -0,0 +1,20 @@
+var index = [
+{'name': 'tests.graph', 'type': 'package', 'url': 'index.html#section-package', 'doc': '', 'tags': [],'icons': ['icon-package']},
+{'name': 'map', 'type': 'function', 'url': 'index.html#map', 'doc': '', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'ColorMapTests.shouldBeWhiteByDefault', 'type': 'function', 'url': 'ColorMapTests.type.html#shouldBeWhiteByDefault', 'doc': '', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'ColorMapTests.shouldSetColor', 'type': 'function', 'url': 'ColorMapTests.type.html#shouldSetColor', 'doc': '', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'ColorMapTests', 'type': 'class', 'url': 'ColorMapTests.type.html', 'doc': '', 'tags': [],'icons': ['icon-class']},
+{'name': 'CycleTests.shouldNotBeEmpty', 'type': 'function', 'url': 'CycleTests.type.html#shouldNotBeEmpty', 'doc': '', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'CycleTests.shouldHaveTenVertices', 'type': 'function', 'url': 'CycleTests.type.html#shouldHaveTenVertices', 'doc': '', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'CycleTests.shouldBeTraversedInOrder', 'type': 'function', 'url': 'CycleTests.type.html#shouldBeTraversedInOrder', 'doc': '', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'CycleTests', 'type': 'class', 'url': 'CycleTests.type.html', 'doc': '', 'tags': [],'icons': ['icon-class']},
+{'name': 'VertexPropagatorTests.shouldPropagateToNeighbors', 'type': 'function', 'url': 'VertexPropagatorTests.type.html#shouldPropagateToNeighbors', 'doc': '', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'VertexPropagatorTests', 'type': 'class', 'url': 'VertexPropagatorTests.type.html', 'doc': '', 'tags': [],'icons': ['icon-class']},
+{'name': 'EdgePropagatorTests.shouldPropagateViaEdges', 'type': 'function', 'url': 'EdgePropagatorTests.type.html#shouldPropagateViaEdges', 'doc': '', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'EdgePropagatorTests.shouldOrientEdgesOutwards', 'type': 'function', 'url': 'EdgePropagatorTests.type.html#shouldOrientEdgesOutwards', 'doc': '', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'EdgePropagatorTests.shouldOrientEdgesOutwards2', 'type': 'function', 'url': 'EdgePropagatorTests.type.html#shouldOrientEdgesOutwards2', 'doc': '', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'EdgePropagatorTests', 'type': 'class', 'url': 'EdgePropagatorTests.type.html', 'doc': '', 'tags': [],'icons': ['icon-class']},
+{'name': 'run', 'type': 'function', 'url': 'index.html#run', 'doc': '<p>Run the module <code>graph.tests</code>.</p>\n', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'tests.graph', 'type': 'module', 'url': 'index.html', 'doc': '', 'tags': [],'icons': []}];
+var tagIndex = [
+];
